cf972a1039ae00b0c752a16927ffb0e4930afda7
Albert Pi <albertpi@gmail.com>
2012-10-11 20:18:54 +0200

    Added featured manager

--- services/managers.py
+++ services/managers.py
@@ -5,6 +5,9 @@ from django.db import models
 class ServiceQuerySet(models.query.QuerySet):
     def live(self):
         return self.filter(status__gte=2)
+    
+    def featured(self):
+        return self.filter(status__gte=3)    
 
 class PublicManager(models.Manager):
     """Returns published featured teasers."""
@@ -16,4 +19,7 @@ class PublicManager(models.Manager):
         
     def live(self, *args, **kwargs):
         return self.get_query_set().live(*args, **kwargs)
+        
+    def featured(self, *args, **kwargs):
+    return self.get_query_set().featured(*args, **kwargs)
     
